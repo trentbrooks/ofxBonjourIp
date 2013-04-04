d0d7a50f7e49221e8574ddcd1e28b3a8e16850f4
trentbrooks <trentbrooks@gmail.com>
2013-04-04 17:53:50 +1100

    first commit- adding src and ios examples

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-ofxBonjourIp
-============
+## ofxBonjourIp ##
+Bonjour sucks. This addon uses Bonjour just to find ip addresses of ios devices on the same network and that's it. Included is a server and client example.
 
-Bonjour sucks. This addon uses Bonjour just to find ip addresses of ios devices on the same network.
+Not setup for network comms- Bonjour streams are ugly. Use OSC, UDP or TCP instead (more OF friendly).
+
+Currently ios, but osx version is on the way. Robotconscience has already done this here though... https://github.com/robotconscience/ofxBonjour
+
+-Trent Brooks
---
+++ ofxBonjourIp/src/ofxBonjourIp.h
@@ -0,0 +1,102 @@
+
+#pragma once
+
+
+#import "ofMain.h"
+#ifdef TARGET_OF_IPHONE
+    #import "ofxiPhoneExtras.h"
+#endif
+//#import <Cocoa/Cocoa.h>
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+
+
+
+class ofxBonjourIp;
+/*
+ ofxBonjourIp created by Trent Brooks.
+ - Using Bonjour services to find an ip address or device name
+ - Not setup for network comms- Bonjour streams are ugly. Use OSC, UDP or TCP instead (more OF friendly).
+*/
+
+// OBJC DELEGATE
+@interface BonjourServiceDelegate : NSObject<NSNetServiceDelegate,NSNetServiceBrowserDelegate> {
+    
+    ofxBonjourIp * bonjourCpp; // giving the delegate access to the OF c++ implementation as well
+    
+	NSNetService *netService; // for server
+    
+    NSNetServiceBrowser *serviceBrowser; // for client
+    NSMutableArray *services; // Keeps track of available services
+    BOOL searching;// Keeps track of search status
+}
+
+- (id) init:(ofxBonjourIp *) bCpp;
+- (void) stopService;
+- (NSString *) getIPAddress;
+
+// server
+- (void) startService:(NSString*)type name:(NSString*)name port:(int)port domain:(NSString*)domain;
+- (void) netService:(NSNetService *)aNetService didNotPublish:(NSDictionary *)dict;
+- (void) netServiceDidPublish:(NSNetService *)sender;
+- (void) netServiceWillPublish:(NSNetService *)sender;
+- (void) handleError:(NSNumber *)error withService:(NSNetService *)service;
+
+//client
+- (void) discoverService:(NSString*)type domain:(NSString*)domain;
+- (void) netServiceBrowserWillSearch:(NSNetServiceBrowser *)browser;
+- (void) netServiceBrowserDidStopSearch:(NSNetServiceBrowser *)browser;
+- (void) netServiceBrowser:(NSNetServiceBrowser *)browser didNotSearch:(NSDictionary *)errorDict;
+- (void) netServiceBrowser:(NSNetServiceBrowser *)browser didFindService:(NSNetService *)aNetService moreComing:(BOOL)moreComing;
+- (void) netServiceBrowser:(NSNetServiceBrowser *)browser didRemoveService:(NSNetService *)aNetService moreComing:(BOOL)moreComing;
+
+- (void) netServiceWillResolve:(NSNetService *)sender;
+- (void) netServiceDidResolveAddress:(NSNetService *)sender;
+- (void) netService:(NSNetService *)sender didNotResolve:(NSDictionary *)errorDict;
+
+//props
+@property(nonatomic, retain) NSNetService *netService;
+@property(nonatomic, retain) NSNetServiceBrowser *serviceBrowser;
+
+@end
+
+
+// OF CLASS
+class ofxBonjourIp {
+	public:
+
+		ofxBonjourIp();
+        ~ofxBonjourIp();
+		
+        BonjourServiceDelegate * bonjourDelegate; // ios delegate
+        bool isServer;
+    
+        // SERVER        
+        void startService( string type, string name, int port, string domain = "" );
+        void stopService();
+        
+        // returns name of this device- can use this to connect via osc
+        string getDeviceHostName();
+        string deviceHostName;
+    
+        // returns ip address of this device- can use this to connect via osc
+        string getDeviceIp();
+        string deviceIp;
+
+    
+        // CLIENT
+        void discoverService( string type, string domain );
+        bool isConnected;
+    
+        // returns name of server- can use this to connect via osc
+        string getServerHostName();
+        string serverHostName;
+    
+        // returns ip address of server- can use this to connect via osc
+        string getServerIp();
+        string serverIp;
+    
+    
+};
+
+
